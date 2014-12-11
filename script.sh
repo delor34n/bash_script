5c1e83dc6850b274248bad23921e0faf2ed9f7a8
delor34n <sebastian.lastra@usach.cl>
2014-12-10 23:38:36 -0300

    first commit

---
+++ script.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+#########################################################
+#           UNIVERSIDAD DE SANTIAGO DE CHILE            #
+#               FACULTAD DE CIENCIA                     #
+#   DPTO. DE MATEMÁTICA Y CIENCIA DE LA COMPUTACIÓN     #
+#       LICENCIATURA EN CIENCIA DE LA COMPUTACIÓN       #
+#                                                       #
+#                                                       #
+# NOMBRE: MARÍA FERNANDA MALDONADO PANTOJAS             #
+# PROFESOR: JORGE URBINA                                #
+#                                                       #
+# SCRIPT QUE BLABLABLABLA...                            #
+#########################################################
+
+
+
